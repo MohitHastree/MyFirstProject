c1928f063d2914dda1130a1f0ca700b89fbb3a57
MohitHastree <mohit.baghel1014@gmail.com>
2024-08-20 17:36:05 +0530

    Initial Commit

---
+++ MyFirstProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirstProject
+//
+//  Created by Apps We Love on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Apps We Love on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
